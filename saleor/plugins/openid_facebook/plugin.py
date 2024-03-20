66965a5c3b98ef5f0b16fe59eb0200eb75425433
fedexin40 <federico.cruz.lozano@hotmail.com>
2024-03-20 07:20:08 -0600

    fix: 🐛 OpenID

--- saleor/plugins/openid_facebook/plugin.py
+++ saleor/plugins/openid_facebook/plugin.py
@@ -1,9 +1,9 @@
 import logging
 
-from django.conf import settings
+from ...permission.enums import get_permissions_codename
+from ..openid_connect.client import OAuth2Client
+from ..openid_connect.const import SALEOR_STAFF_PERMISSION
 from ..openid_connect.plugin import OpenIDConnectPlugin
-from ..openid_connect.dataclasses import OpenIDConnectConfig
-
 from . import PLUGIN_ID
 
 logger = logging.getLogger(__name__)
@@ -14,35 +14,18 @@ class OpenIDConnectPluginFacebook(OpenIDConnectPlugin):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Convert to dict to easier take config elements
-        self.config = OpenIDConnectConfig(
-            client_id=settings.CLIENT_ID_FACEBOOK,
-            client_secret=settings.CLIENT_SECRET_FACEBOOK,
-            enable_refresh_token=settings.ENABLE_REFRESH_TOKEN_FACEBOOK,
-            json_web_key_set_url=settings.JSON_WEB_KEY_SET_URL_FACEBOOK,
-            authorization_url=settings.AUTHORIZATION_URL_FACEBOOK,
-            token_url=settings.TOKEN_URL_FACEBOOK,
-            user_info_url=settings.USER_INFO_URL_FACEBOOK,
-            audience = '',
-            use_scope_permissions = False,
-            staff_user_domains = '',
-            default_group_name = '',
-            logout_url = '',
-        )
 
-        # Determine, if we have defined all fields required to use OAuth access token
-        # as Saleor's authorization token.
-        self.use_oauth_access_token = bool(
-            self.config.user_info_url and self.config.json_web_key_set_url
+    def _get_oauth_session(self):
+        scope = "email"
+        if self.config.use_scope_permissions:
+            permissions = [f"saleor:{perm}" for perm in get_permissions_codename()]
+            permissions.append(SALEOR_STAFF_PERMISSION)
+            scope_permissions = " ".join(permissions)
+            scope += f" {scope_permissions}"
+        if self.config.enable_refresh_token:
+            scope += " offline_access"
+        return OAuth2Client(
+            client_id=self.config.client_id,
+            client_secret=self.config.client_secret,
+            scope=scope,
         )
-
-        # Determine, if we have defined all fields required to process the
-        # authorization flow.
-        self.use_authorization_flow = bool(
-            self.config.json_web_key_set_url
-            and self.config.authorization_url
-            and self.config.token_url
-        )
-
-        # Activating the plugin manually
-        self.active = True
--- saleor/plugins/openid_google/plugin.py
+++ saleor/plugins/openid_google/plugin.py
@@ -1,9 +1,6 @@
 import logging
 
-from django.conf import settings
 from ..openid_connect.plugin import OpenIDConnectPlugin
-from ..openid_connect.dataclasses import OpenIDConnectConfig
-
 from . import PLUGIN_ID
 
 logger = logging.getLogger(__name__)
@@ -14,35 +11,3 @@ class OpenIDConnectPluginGoogle(OpenIDConnectPlugin):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Convert to dict to easier take config elements
-        self.config = OpenIDConnectConfig(
-            client_id=settings.CLIENT_ID_GOOGLE,
-            client_secret=settings.CLIENT_SECRET_GOOGLE,
-            enable_refresh_token=settings.ENABLE_REFRESH_TOKEN_GOOGLE,
-            json_web_key_set_url=settings.JSON_WEB_KEY_SET_URL_GOOGLE,
-            authorization_url=settings.AUTHORIZATION_URL_GOOGLE,
-            token_url=settings.TOKEN_URL_GOOGLE,
-            user_info_url=settings.USER_INFO_URL_GOOGLE,
-            audience = '',
-            use_scope_permissions = False,
-            staff_user_domains = '',
-            default_group_name = '',
-            logout_url = '',
-        )
-
-        # Determine, if we have defined all fields required to use OAuth access token
-        # as Saleor's authorization token.
-        self.use_oauth_access_token = bool(
-            self.config.user_info_url and self.config.json_web_key_set_url
-        )
-
-        # Determine, if we have defined all fields required to process the
-        # authorization flow.
-        self.use_authorization_flow = bool(
-            self.config.json_web_key_set_url
-            and self.config.authorization_url
-            and self.config.token_url
-        )
-
-        # Activating the plugin manually
-        self.active = True
